2ebd96c28af92be4251b5ca1bf7c3cb7659b9cc3
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-11-11 16:04:13 +0800

    Updated emoji-fallback.js 📥

--- src/emoji-fallback.js
+++ src/emoji-fallback.js
@@ -42,6 +42,29 @@ export const emojiSupported = () => {
 };
 
 /**
+ * Checks if the browser supports national flag emoji rendering.
+ * @returns {boolean} True if flags are supported, false otherwise.
+ */
+export const flagSupported = () => {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx || typeof ctx.fillText !== 'function') {
+    canvas.remove();
+    return false;
+  }
+
+  ctx.textBaseline = 'top';
+  ctx.font = '32px Arial';
+  ctx.fillText('\u{1f1fa}\u{1f1f8}', 0, 0);
+
+  let imageData = ctx.getImageData(16, 16, 1, 1).data;
+  imageData = imageData[0] !== 0;
+  canvas.remove();
+  return imageData;
+};
+
+/**
  * Replaces Unicode emojis with image emojis using Twemoji if native emoji support is not available.
  * @param {Element|DocumentFragment} [element=document.body] - The DOM element to parse for emojis.
  * @param {string} [cdn='https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/'] - The CDN URL for Twemoji assets.
@@ -63,17 +86,22 @@ export const parseEmoji = async (element = document.body, cdn = 'https://cdn.jsd
   }  
   
   const emojiSupport = emojiSupported();
+  const flagSupport = emojiSupport && flagSupported();
   
-  if (emojiSupport) {
+  if (flagSupport) {
     return;
   }
   
-  if (!emojiSupport) {
-    return await twemoji.parse(element, {
-      base: cdn,
-      className: className
-    });
-  }
+  return await twemoji.parse(element, {
+    callback: flagSupport ? undefined : (icon, options, variant) => {
+      if (!icon.match(/^1f1[ef][0-9a-f]-1f1[ef][0-9a-f]$/)) {
+        return;
+      }
+      return ''.concat(options.base, options.size, '/', icon, options.ext);
+    },
+    base: cdn,
+    className: className
+  });
 };
 
 
